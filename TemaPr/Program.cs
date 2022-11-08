61f09bb55a47db74687ee14bbf6b48f26ca0219a
Бр <@BRUH>
2022-11-08 19:50:53 +0300

    Add project files.

---
+++ TemaPr/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace TemaPr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool end = false;
+            do
+            {
+                Console.WriteLine("ВВедите слогаемое 1");
+                string a = Console.ReadLine();
+
+                Console.WriteLine("ВВедите слогаемое 2");
+                string b = Console.ReadLine();
+
+                int.TryParse(a, out int cal);
+                int.TryParse(b, out int vruh);
+
+                if (cal == 0 && vruh == 0)
+                    Console.WriteLine("Введите цифры");
+                else
+                {
+                    Console.WriteLine($"{cal} + {vruh} = {cal + vruh}");
+                }
+
+                Console.WriteLine("Повторить?");
+                Console.WriteLine("Y/N");
+               if(Console.ReadLine() != "Y")
+                    end = true;
+               else
+                Console.Clear();
+            } while (end == false);
+            
+        }
+    }
+}
